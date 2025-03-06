88630f4109bffda8f138f090ba4c5aaf40cf4a5f
shubhampawar4841 <shubhampawar4036@gmail.com>
2025-03-06 10:35:16 +0530

    laoding

---
+++ app/dashboard/cards.tsx
@@ -0,0 +1,24 @@
+impor
+
+
+export default async function CardWrapper() {
+    const {
+      numberOfInvoices,
+      numberOfCustomers,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    } = await fetchCardData();
+   
+    return (
+      <>
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </>
+    );
+  }
